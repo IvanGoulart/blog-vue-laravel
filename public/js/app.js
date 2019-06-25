b27ccdd3a00be55d153b04a1f70ffe4c3d84852f
Ivan Goulart <you@example.com>
2019-06-25 20:49:54 -0300

    Incluir ordenação

--- public/js/app.js
+++ public/js/app.js
@@ -45623,20 +45623,57 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-    props: ['titulos', 'itens', 'criar', 'detalhe', 'editar', 'deletar', 'token'],
+    props: ['titulos', 'itens', 'ordem', 'ordemcol', 'criar', 'detalhe', 'editar', 'deletar', 'token'],
     data: function data() {
         return {
-            buscar: ''
+            buscar: '',
+            ordemAux: this.ordem || "asc",
+            ordemAuxCol: this.ordemcol || 0
         };
     },
     methods: {
         executaForm: function executaForm(index) {
             document.getElementById('index').submit();
+        },
+        ordenaColuna: function ordenaColuna(coluna) {
+            this.ordemAuxCol = coluna;
+            if (this.ordemAux.toLowerCase() == "asc") {
+                this.ordemAux = 'desc';
+            } else {
+                this.ordemAux = 'asc';
+            }
         }
     },
     computed: {
         lista: function lista() {
             var _this = this;
+
+            var ordem = this.ordemAux;
+            var ordemCol = this.ordemAuxCol;
+            ordem = ordem.toLowerCase();
+            ordemCol = parseInt(ordemCol);
+
+            if (ordem == "desc") {
+                this.itens.sort(function (a, b) {
+                    if (a[ordemCol] < b[ordemCol]) {
+                        return 1;
+                    }
+                    if (a[ordemCol] > b[ordemCol]) {
+                        return -1;
+                    }
+                    return 0;
+                });
+            } else {
+                this.itens.sort(function (a, b) {
+                    if (a[ordemCol] > b[ordemCol]) {
+                        return 1;
+                    }
+                    if (a[ordemCol] < b[ordemCol]) {
+                        return -1;
+                    }
+                    return 0;
+                });
+            }
 
             return this.itens.filter(function (res) {
                 for (var k = 0; k < res.length; k++) {
@@ -45697,7 +45734,19 @@ var render = function() {
           "tr",
           [
             _vm._l(_vm.titulos, function(titulo, index) {
-              return _c("th", { key: index }, [_vm._v(_vm._s(titulo))])
+              return _c(
+                "th",
+                {
+                  key: index,
+                  staticStyle: { cursor: "pointer" },
+                  on: {
+                    click: function($event) {
+                      return _vm.ordenaColuna(index)
+                    }
+                  }
+                },
+                [_vm._v(_vm._s(titulo))]
+              )
             }),
             _vm._v(" "),
             _vm.detalhe || _vm.editar || _vm.deletar
--- resources/assets/js/components/TabelaLista.vue
+++ resources/assets/js/components/TabelaLista.vue
@@ -10,7 +10,7 @@
         <table class="table table-striped table-hover">
             <thead>
                 <tr>
-                    <th v-for="(titulo, index) in titulos" v-bind:key="index">{{titulo}}</th>
+                    <th style="cursor:pointer" v-on:click="ordenaColuna(index)" v-for="(titulo, index) in titulos" v-bind:key="index">{{titulo}}</th>
                     <th v-if="detalhe || editar || deletar">Ação</th>
                 </tr>
             </thead>
@@ -49,19 +49,53 @@
 
 <script>
     export default {
-        props: ['titulos', 'itens', 'criar', 'detalhe', 'editar', 'deletar', 'token' ],
+        props: ['titulos', 'itens','ordem', 'ordemcol', 'criar', 'detalhe', 'editar', 'deletar', 'token' ],
         data: function(){
             return{
                 buscar:'',
+                ordemAux: this.ordem || "asc",
+                ordemAuxCol: this.ordemcol || 0
             }
         },
         methods: {
             executaForm: function(index){
                 document.getElementById('index').submit();
+            },
+            ordenaColuna(coluna){
+                this.ordemAuxCol = coluna;
+                if(this.ordemAux.toLowerCase() == "asc"){
+                    this.ordemAux = 'desc';
+                }else{
+                    this.ordemAux = 'asc';
+                }
             }
         },
         computed: {
             lista:function(){
+
+                let ordem = this.ordemAux;
+                let ordemCol = this.ordemAuxCol;
+                ordem = ordem.toLowerCase();
+                ordemCol = parseInt(ordemCol);
+
+                if(ordem == "desc"){
+                    this.itens.sort(function(a,b){
+                        if (a[ordemCol] < b[ordemCol]) {return 1;}
+                        if (a[ordemCol] > b[ordemCol]) {return -1;}
+                        return 0;
+                    });
+
+                }else{
+                    this.itens.sort(function(a,b){
+                        if (a[ordemCol] > b[ordemCol]) {return 1;}
+                        if (a[ordemCol] < b[ordemCol]) {return -1;}
+                        return 0;
+                    });
+
+                }
+
+
+
                 return this.itens.filter(res => {
                     for(let k = 0; k < res.length; k++){
                         if((res[k] + "").toLowerCase().indexOf(this.buscar.toLowerCase()) >= 0){
