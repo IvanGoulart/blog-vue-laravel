25c9880a23c0ce8f8a66d5e24df2ff3398e3742c
Ivan <ivan_goulart@hotmail.com>
2020-01-08 21:01:38 -0300

    Criação do home

---
+++ resources/views/site.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+  <pagina tamanho="12">
+    <painel titulo="Artigos">
+      <div class="row">
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="https://s3.amazonaws.com/uploads.hotmart.com/blog/2017/09/criar-um-blog-670x419.png" alt="...">
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Teste de descrição</p>
+              <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="https://s3.amazonaws.com/uploads.hotmart.com/blog/2017/09/criar-um-blog-670x419.png" alt="...">
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Teste de descrição</p>
+              <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+            </div>
+          </div>
+        </div>
+     
+
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="https://s3.amazonaws.com/uploads.hotmart.com/blog/2017/09/criar-um-blog-670x419.png" alt="...">
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Teste de descrição</p>
+              <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img src="https://s3.amazonaws.com/uploads.hotmart.com/blog/2017/09/criar-um-blog-670x419.png" alt="...">
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>Teste de descrição</p>
+              <p><a href="#" class="btn btn-primary" role="button">Leia mais</a></p>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </painel>
+  </pagina>
+@endsection
